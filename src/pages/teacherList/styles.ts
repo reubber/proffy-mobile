6805aee822f51f88065eb730600a1d0ede0ab787
Reubber <reubbers@alu.ufc.br>
2020-08-15 23:28:12 -0300

    add button style on filters submit at teacher page

--- src/pages/teacherList/styles.ts
+++ src/pages/teacherList/styles.ts
@@ -1,16 +1,17 @@
-import {StyleSheet} from 'react-native'
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor:'#f0f0f7'
+    flex: 1,
+    backgroundColor: '#f0f0f7'
   },
+
   teacherList: {
     marginTop: -40,
   },
 
   searchForm: {
-    marginBottom: 8
+    marginBottom: 24,
   },
 
   label: {
@@ -18,25 +19,39 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins_400Regular'
   },
 
+  input: {
+    height: 54,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16,
+  },
+
   inputGroup: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
 
   inputBlock: {
     width: '48%'
   },
 
-  input: {
-    height: 54,
-    backgroundColor: '#FFF',
+  submitButton: {
+    backgroundColor: '#04d361',
+    height: 56,
+    flexDirection: 'row',
     borderRadius: 8,
+    alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 16,
-    marginTop: 4,
-    marginBottom: 16
-  }
-  
-})
+  },
 
-export default styles
+  submitButtonText: {
+    color: '#fff',
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 16,
+  },
+});
+
+export default styles;
