a0803d4fea7a4623dda09eafb4aec91310a36b7c
Reubber <reubbers@alu.ufc.br>
2020-08-18 23:52:05 -0300

    add favorited TecherItem compomnent

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, ScrollView } from 'react-native'
+import {useFocusEffect} from '@react-navigation/native'
 import PageHeader from '../../components/pageHeader'
 
 import styles from './styles'
-import TeacherItem from '../../components/teacherItem'
+import TeacherItem, { Teacher} from '../../components/teacherItem'
+import AsyncStorage from '@react-native-community/async-storage'
 
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([])
+
+  function loadFavorites () {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response)
+
+        setFavorites(favoritedTeachers)
+      }
+    })
+  }
+
+  useFocusEffect(() => {
+    loadFavorites()
+  },)
+
   return (
     <View style={styles.container}>
       <PageHeader title='Meus Proffys favoritos'/>
@@ -18,11 +36,15 @@ function Favorites() {
         paddingBottom: 16
       }}
     >  
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
+      {favorites.map((teacher: Teacher) => {
+        return (
+          <TeacherItem
+            key={teacher.id}
+            teacher={teacher}
+            favorited 
+          />
+        )
+      })}
     </ScrollView>
 
     </View>
